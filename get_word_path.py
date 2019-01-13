9b860e836f7cd8a757c0665987a29e3a18afc81e
SanfordLuo <37099097+SanfordLuo@users.noreply.github.com>
2019-01-13 17:34:24 +0800

    检测目录文件夹dirPath下的所有py文件中是否存在关键字word，以及返回所在的py文件
    检测目录文件夹dirPath下的所有py文件中是否存在关键字word，以及返回所在的py文件

---
+++ get_word_path.py
@@ -0,0 +1,30 @@
+"""
+检测目录文件夹dirPath下的所有py文件中是否存在关键字word，以及返回所在的py文件
+"""
+import os
+
+dirPath = "../GetPath/script"
+word = "jay"
+
+def get_word_path(dirPath, word):
+    status = False
+    path_list = []
+    for root, dirs, files in os.walk(dirPath):
+        for file in files:
+            if os.path.splitext(file)[1] == '.py':
+                file_path = os.path.join(root, file)
+                path_list.append(file_path)
+
+    for file in path_list:
+        f = open(file, 'r', encoding="utf-8")
+        if word in f.read():
+            print("%s关键字在文件%s" % (word, file))
+            status = True
+        f.close()
+
+    if not status:
+        print("%s关键字不存在" % word)
+
+
+if __name__ == '__main__':
+    get_word_path(dirPath, word)
