95e6a5b52d779e9d4ec151f2af51dd8106847410
SanfordLuo <37099097+SanfordLuo@users.noreply.github.com>
2019-05-26 22:06:18 +0800

    单例
    单例

---
+++ singleton.py
@@ -0,0 +1,13 @@
+class Singleton(object):
+    _instance = None
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = object.__new__(cls)
+            return cls._instance
+        else:
+            return cls._instance
+
+s1 = Singleton()
+s2 = Singleton()
+print(s1)   # <__main__.Singleton object at 0x000002223BBDB278>
+print(s2)   # <__main__.Singleton object at 0x000002223BBDB278>
