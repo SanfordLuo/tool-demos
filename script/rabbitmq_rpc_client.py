4f8fb0b6b2d6847f71979321f4ee2f7542914321
SanfordLuo <864938527@qq.com>
2022-01-20 21:21:49 +0800

    rpc服务

---
+++ script/rabbitmq_rpc_client.py
@@ -0,0 +1,42 @@
+import pika
+import uuid
+
+
+class FibonacciRpcClient(object):
+    def __init__(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+
+        self.channel = self.connection.channel()
+
+        result = self.channel.queue_declare(queue='', exclusive=True)
+        self.callback_queue = result.method.queue
+
+        self.channel.basic_consume(queue=self.callback_queue,
+                                   on_message_callback=self.on_response,
+                                   auto_ack=True)
+
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    def call(self, n):
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(exchange='',
+                                   routing_key='rpc_queue',
+                                   properties=pika.BasicProperties(
+                                       reply_to=self.callback_queue,
+                                       correlation_id=self.corr_id,
+                                   ),
+                                   body=str(n))
+        while self.response is None:
+            self.connection.process_data_events()
+        return int(self.response)
+
+
+if __name__ == '__main__':
+    fibonacci_rpc = FibonacciRpcClient()
+
+    print("开始发起rpc请求......")
+    response = fibonacci_rpc.call(30)
+    print("rpc服务计算结果 response: {0}".format(response))
