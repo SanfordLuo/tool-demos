4ce876f105a3b7c156a754f5e9b442314e915c51
SanfordLuo <864938527@qq.com>
2022-01-21 11:40:50 +0800

    rpc

--- script/rabbitmq_rpc_client.py
+++ script/rabbitmq_rpc_client.py
@@ -1,42 +1,73 @@
 import pika
 import uuid
+import json
 
 
-class FibonacciRpcClient(object):
+class BubbleSortRpcClient(object):
     def __init__(self):
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        self.username = 'sanford'
+        self.password = '123456'
+        self.host = 'localhost'
+        self.port = 5672
+        self.virtual_host = 'sanford_host'
 
+        credentials = pika.PlainCredentials(username=self.username, password=self.password)
+        params = pika.ConnectionParameters(host=self.host,
+                                           port=self.port,
+                                           virtual_host=self.virtual_host,
+                                           credentials=credentials)
+
+        # 创建连接
+        self.connection = pika.BlockingConnection(params)
+
+        # 创建信道
         self.channel = self.connection.channel()
 
-        result = self.channel.queue_declare(queue='', exclusive=True)
+        # 声明一个队列用于接收服务端返回结果, exclusive=True 只允许当前连接访问
+        result = self.channel.queue_declare(queue='', exclusive=True, durable=True)
         self.callback_queue = result.method.queue
 
+        # 接收服务端返回的结果进行消费
         self.channel.basic_consume(queue=self.callback_queue,
-                                   on_message_callback=self.on_response,
-                                   auto_ack=True)
+                                   on_message_callback=self.on_response)
 
     def on_response(self, ch, method, props, body):
+        """
+        接收到服务端返回的结果的回调函数
+        """
         if self.corr_id == props.correlation_id:
-            self.response = body
+            if isinstance(body, str):
+                self.response = body
+            else:
+                self.response = json.loads(body)
 
-    def call(self, n):
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    def call(self, ls):
         self.response = None
         self.corr_id = str(uuid.uuid4())
+
+        # 请求消息属性设置 reply_to:接收服务端消息的队列
+        properties = pika.BasicProperties(delivery_mode=2,
+                                          reply_to=self.callback_queue,
+                                          correlation_id=self.corr_id)
+        # 发送请求消息
         self.channel.basic_publish(exchange='',
                                    routing_key='rpc_queue',
-                                   properties=pika.BasicProperties(
-                                       reply_to=self.callback_queue,
-                                       correlation_id=self.corr_id,
-                                   ),
-                                   body=str(n))
+                                   body=json.dumps(ls),
+                                   properties=properties)
+
         while self.response is None:
+            # 保持连接 检查是否有回调
             self.connection.process_data_events()
-        return int(self.response)
+        return self.response
 
 
 if __name__ == '__main__':
-    fibonacci_rpc = FibonacciRpcClient()
+    bubble_sort = BubbleSortRpcClient()
 
-    print("开始发起rpc请求......")
-    response = fibonacci_rpc.call(30)
-    print("rpc服务计算结果 response: {0}".format(response))
+    ls = [1, 22, 3, 7, 4, 77, 34, 66, 5, 4, 8, 15]
+    # ls = '2223'
+    print("客户端开始发起请求......")
+    response = bubble_sort.call(ls)
+    print("服务端计算结果 response: {0}".format(response))
--- script/rabbitmq_rpc_service.py
+++ script/rabbitmq_rpc_service.py
@@ -1,37 +1,62 @@
 import pika
+import json
 
 
-def fib(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fib(n - 1) + fib(n - 2)
+def bubble_sort(ls):
+    """
+    冒泡排序
+    """
+    for i in range(len(ls) - 1):
+        for j in range(len(ls) - i - 1):
+            if ls[j] > ls[j + 1]:
+                ls[j], ls[j + 1] = ls[j + 1], ls[j]
+    return ls
 
 
 def on_request(ch, method, props, body):
-    n = int(body)
+    ls = json.loads(body)
 
-    print("rpc请求参数 n:{0}".format(n))
-    response = fib(n)
-    print("rpc服务计算结果 response:{0}".format(response))
+    print("客户端请求参数 ls:{0}".format(ls))
 
+    if isinstance(ls, list):
+        response = bubble_sort(ls)
+    else:
+        response = '客户端请求参数类型不对 type:{0}'.format(type(ls))
+    print("服务端计算结果 response:{0}".format(response))
+
+    # 服务端发送消息的属性设置
+    properties = pika.BasicProperties(delivery_mode=2,
+                                      correlation_id=props.correlation_id)
+
+    # 服务端发送计算后的结果, routing_key为客户端传来的reply_to
     ch.basic_publish(exchange='',
                      routing_key=props.reply_to,
-                     properties=pika.BasicProperties(correlation_id=props.correlation_id),
-                     body=str(response))
+                     properties=properties,
+                     body=json.dumps(response))
+
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
 if __name__ == '__main__':
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    username = 'sanford'
+    password = '123456'
+    host = 'localhost'
+    port = 5672
+    virtual_host = 'sanford_host'
+
+    credentials = pika.PlainCredentials(username=username, password=password)
+    params = pika.ConnectionParameters(host=host,
+                                       port=port,
+                                       virtual_host=virtual_host,
+                                       credentials=credentials)
+    connection = pika.BlockingConnection(params)
     channel = connection.channel()
-    channel.queue_declare(queue='rpc_queue')
+
+    channel.queue_declare(queue='rpc_queue', durable=True)
 
     channel.basic_qos(prefetch_count=1)
 
     channel.basic_consume(queue='rpc_queue', on_message_callback=on_request)
 
-    print("正在等待rpc请求......")
+    print("正在等待客户端请求......")
     channel.start_consuming()
